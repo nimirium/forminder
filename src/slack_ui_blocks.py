39d4050c0af7449bbd65270257803ab77e10a463
Sophie Fogel <sophie@sensibo.com>
2023-04-10 14:40:51 +0300

    change "create form" description

--- src/slack_ui_blocks.py
+++ src/slack_ui_blocks.py
@@ -122,16 +122,18 @@ help_text_block = {
 
 form_create_help_text = f""":information_desk_person: To create a form, follow these steps:
 
-:zap: Type: */{SLASH_COMMAND} create [options]* :zap:
+:zap: Type: /{SLASH_COMMAND} create [options] :zap:
 
-:jigsaw: *Options:* :jigsaw:
-- Use *--form-name* followed by the form name you want.
-- To add a single-line text field, use *--text-field* followed by the field name.
-- For a multi-line text field, use *--multiline-field* followed by the field name.
-- To add a dropdown menu, use *--select-field* followed by the field name and the available options separated by commas.
+:jigsaw: Options: :jigsaw:
+Select a name
+    Use --form-name followed by the form name you want.
+Add fields
+    - To add a single-line text field, use --text-field followed by the field name.
+    - For a multi-line text field, use --multiline-field followed by the field name.
+    - To add a dropdown menu, use --select-field followed by the field name and the available options separated by commas.
 
-:airplane: *Example:* :airplane:
-/{SLASH_COMMAND} create --form-name="Project Update" --text-field="Task Name" --select-field="Progress:Not Started,In Progress,Completed" --multiline-field="Notes or Challenges"
+:airplane: Example: :airplane:
+/{SLASH_COMMAND} create --form-name=“Project Update” --text-field=“Task Name” --select-field=“Progress:Not Started,In Progress,Completed” --multiline-field=“Notes or Challenges”
 """
 
 
