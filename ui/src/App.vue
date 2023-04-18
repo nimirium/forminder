1b6d3b5957372bbdb0c5fafb2252628797c3883b
Sophie Fogel <sophie@sensibo.com>
2023-04-19 00:09:37 +0300

    home view looks good

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,82 +4,51 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <div id="app-container" class="min-h-screen flex flex-col">
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <br/>
-        <RouterLink to="/about">About</RouterLink>
-        <br/>
-        <RouterLink to="/login">Log in</RouterLink>
-      </nav>
+    <div class="p-8 md:p-0">
+    <header>
+
+        <nav>
+
+          <div class="flex justify-center items-center">
+            <RouterLink to="/" class="px-3 md:px-5 py-3">Home</RouterLink>
+            <RouterLink to="/login" class="px-3 md:px-5 py-3">Log in</RouterLink>
+            <RouterLink to="/add-to-slack" class="px-3 md:px-5 py-3 hidden md:block">Add to Slack</RouterLink>
+            <RouterLink to="/how-to-use" class="px-3 md:px-5 py-3 hidden md:block">How to use</RouterLink>
+            <RouterLink to="/use-cases" class="px-3 md:px-5 py-3 hidden md:block">Use cases</RouterLink>
+          </div>
+
+        </nav>
+      </header>
     </div>
-  </header>
 
-  <RouterView />
+
+    <div class="flex-grow">
+      <RouterView/>
+    </div>
+
+    <div class="py-3">
+      <footer>
+
+        <nav>
+
+          <div class="flex justify-center items-center flex-col md:flex-row">
+            <RouterLink to="/how-to-use" class="px-3 md:px-5 py-1 md:py-3 md:hidden">How to use</RouterLink>
+            <RouterLink to="/use-cases" class="px-3 md:px-5 py-1 md:py-3 md:hidden">Use cases</RouterLink>
+            <RouterLink to="/about" class="px-3 md:px-5 py-1 md:py-3">About</RouterLink>
+            <RouterLink to="/terms-and-conditions" class="px-3 md:px-5 py-1 md:py-3">Terms and Conditions</RouterLink>
+          </div>
+
+        </nav>
+
+      </footer>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-/*header {*/
-/*  line-height: 1.5;*/
-/*  max-height: 100vh;*/
-/*}*/
-
-/*.logo {*/
-/*  display: block;*/
-/*  margin: 0 auto 2rem;*/
-/*}*/
-
-/*nav {*/
-/*  width: 100%;*/
-/*  font-size: 12px;*/
-/*  text-align: center;*/
-/*  margin-top: 2rem;*/
-/*}*/
-
-/*nav a.router-link-exact-active {*/
-/*  color: var(--color-text);*/
-/*}*/
-
-/*nav a.router-link-exact-active:hover {*/
-/*  background-color: transparent;*/
-/*}*/
-
-/*nav a {*/
-/*  display: inline-block;*/
-/*  padding: 0 1rem;*/
-/*  border-left: 1px solid var(--color-border);*/
-/*}*/
-
-/*nav a:first-of-type {*/
-/*  border: 0;*/
-/*}*/
-
-/*@media (min-width: 1024px) {*/
-/*  header {*/
-/*    display: flex;*/
-/*    place-items: center;*/
-/*    padding-right: calc(var(--section-gap) / 2);*/
-/*  }*/
-
-/*  .logo {*/
-/*    margin: 0 2rem 0 0;*/
-/*  }*/
-
-/*  header .wrapper {*/
-/*    display: flex;*/
-/*    place-items: flex-start;*/
-/*    flex-wrap: wrap;*/
-/*  }*/
-
-/*  nav {*/
-/*    text-align: left;*/
-/*    margin-left: -1rem;*/
-/*    font-size: 1rem;*/
-
-/*    padding: 1rem 0;*/
-/*    margin-top: 1rem;*/
-/*  }*/
-/*}*/
+.router-link-active {
+  font-weight: bold;
+}
 </style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -6,9 +6,25 @@ import router from './router'
 
 import './assets/main.css'
 
+/* import the fontawesome core */
+// @ts-ignore
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+// @ts-ignore
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+// @ts-ignore
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faBars);
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
