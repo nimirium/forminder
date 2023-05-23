5368b0639c30f030b1f3c9224cfff0cc829ecd88
Sophie Fogel <sophie@sensibo.com>
2023-05-23 12:01:56 +0300

    ...

--- ui/src/views/SubmissionsView.vue
+++ ui/src/views/SubmissionsView.vue
@@ -8,7 +8,7 @@
   </div>
   <div>
     <div class="flex justify-around">
-      <div>
+      <div v-if="!loading">
         <span>
           Export as
         </span>
@@ -39,7 +39,7 @@
       />
 
       <div
-          v-if="!submissions.length"
+          v-if="!loading && !submissions.length"
           class="mygrid-item opacity-75 p-7 rounded-xl shadow-md w-full md:w-1/3 lg:w-1/4 p-4"
       >
         <div class="p-1">
@@ -61,19 +61,19 @@
     </div>
 
     <!-- Pagination -->
-    <div class="flex justify-center">
+    <div v-if="!loading" class="flex justify-center">
       <div class="m-3">
         <button
             v-if="page > 1"
             @click="changePage(1)"
-            class="p-3"
+            class="p-3 underline"
         >
           First
         </button>
         <button
             v-if="page > 1"
             @click="changePage(page - 1)"
-            class="p-3"
+            class="p-3 underline"
         >
           Previous
         </button>
@@ -83,14 +83,14 @@
         <button
             v-if="page < lastPage"
             @click="changePage(page + 1)"
-            class="p-3"
+            class="p-3 underline"
         >
           Next
         </button>
         <button
             v-if="page < lastPage"
             @click="changePage(lastPage)"
-            class="p-3"
+            class="p-3 underline"
         >
           Last
         </button>
@@ -128,8 +128,8 @@ export default defineComponent({
     const downloadData = () => {
       const exportUrl =
         exportType.value === "csv"
-          ? apiURL + "/api/v1/submissions/export/csv"
-          : apiURL + "/api/v1/submissions/export/xlsx";
+          ? apiURL + "/api/v1/submissions/export/csv?formId=" + formId
+          : apiURL + "/api/v1/submissions/export/xlsx?formId=" + formId;
       window.location.href = exportUrl;
     };
 
@@ -142,137 +142,9 @@ export default defineComponent({
 
       if (response.ok) {
         const data = await response.json();
-        // const data = {
-        //   "form_name": "Dogs form",
-        //   "page": 1,
-        //   "per_page": 10,
-        //   "submissions": [{
-        //     "created_at": "2023-04-03T10:35:42.103000",
-        //     "fields": [{"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}, {
-        //       "title": "Dog name",
-        //       "value": "Doggie"
-        //     }, {"title": "Is your dog friendly with other dogs?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 3, 2023",
-        //     "formatted_time": "10:35",
-        //     "id": "642aabfe7af57bab8b74f38d",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:54:31.048000",
-        //     "fields": [{"title": "Dog name", "value": "sfsdf"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "fhgfgh"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "No"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:54",
-        //     "id": "643d6bb7f02800015e0d3c44",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:54:45.590000",
-        //     "fields": [{"title": "Dog name", "value": "ertert"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "asdasd"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "No"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:54",
-        //     "id": "643d6bc5f02800015e0d3c45",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:55:18.489000",
-        //     "fields": [{"title": "Dog name", "value": "etert"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "fgdgd"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:55",
-        //     "id": "643d6be6f02800015e0d3c46",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:55:31.831000",
-        //     "fields": [{"title": "Dog name", "value": "ertret"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "qw"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:55",
-        //     "id": "643d6bf3f02800015e0d3c47",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:55:48.752000",
-        //     "fields": [{"title": "Dog name", "value": "yrtyrt"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "ghjghj"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "No"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:55",
-        //     "id": "643d6c04f02800015e0d3c48",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:56:00.230000",
-        //     "fields": [{"title": "Dog name", "value": "qweqwe"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "fghfgh"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:56",
-        //     "id": "643d6c10f02800015e0d3c49",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:56:20.480000",
-        //     "fields": [{"title": "Dog name", "value": "trty"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "fghfgh"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:56",
-        //     "id": "643d6c24f02800015e0d3c4a",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:56:29.305000",
-        //     "fields": [{"title": "Dog name", "value": "wrwer"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "fghfgh"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:56",
-        //     "id": "643d6c2df02800015e0d3c4b",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }, {
-        //     "created_at": "2023-04-17T18:56:39.064000",
-        //     "fields": [{"title": "Dog name", "value": "qweqwe"}, {
-        //       "title": "Is your dog friendly with other dogs?",
-        //       "value": "ertret"
-        //     }, {"title": "Are you bringing your dog to work tomorrow?", "value": "Yes"}],
-        //     "form_id": "642aabdb1ef24e18c6d95fab",
-        //     "formatted_date": "Apr. 17, 2023",
-        //     "formatted_time": "18:56",
-        //     "id": "643d6c37f02800015e0d3c4c",
-        //     "user_id": "U02RCUYKPK4",
-        //     "user_name": "sophie.fogel"
-        //   }],
-        //   "total": 11
-        // };
-
         formName.value = data.form_name;
         submissions.value = data.submissions;
-        lastPage.value = data.total;
+        lastPage.value = Math.floor(data.total / perPage.value) + (data.total % perPage.value > 0 ? 1 : 0);
       }
     };
 
